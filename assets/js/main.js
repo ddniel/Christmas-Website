171b9fb163875f5d904d6636a4b70d1f9ee5e586
Danny Becerra <ddnielbec@outlook.com>
2022-02-01 17:31:45 -0500

    Inicio un poco avanzado

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/* ============== MOSTRAR MENU ============== */
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/* ============== REMOVER MENU MOVIL ============== */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* ============== CAMBIAR BACKGROUND DEL MENU AL DESLIZAR ============== */
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50)
+      header.classList.add('scroll-header');
+    else
+      header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/* ============== NEW SWIPER ============== */
+
+/* ============== SCROLL SECTIONS ACTIVE LINK ============== */
+
+/* ============== MOSTRAR VOLVER ARRIBA ============== */
+
+/* ============== DARK LIGHT THEME ============== */
