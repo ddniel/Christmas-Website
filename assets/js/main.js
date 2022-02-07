b039358570358619a58401bae8a25ce35a10c29f
Danny Becerra <ddnielbec@outlook.com>
2022-02-07 17:28:57 -0500

    New Section y sistema de swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,23 @@ window.addEventListener('scroll', scrollHeader)
 
 /* ============== NEW SWIPER ============== */
 
+let newSwiper = new Swiper(".new-swiper", {  //Esto sale de la pagina de swiperjs
+        spaceBetween: 24,
+        loop: 'true',
+        slidesPerView: 'auto',
+        centeredSlides: true,
+
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true
+        },
+        breakpoints: {
+          992: {
+            spaceBetween: 80,
+          },
+        },
+      });
+
 /* ============== SCROLL SECTIONS ACTIVE LINK ============== */
 
 /* ============== MOSTRAR VOLVER ARRIBA ============== */
