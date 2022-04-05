20f65f3159e4304abb9d7746bb48286d06760f9f
Danny Becerra <ddnielbec@outlook.com>
2022-04-05 17:28:10 -0500

    Efecto de aparecer al hacer scroll y Finalizacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,17 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 /* ============== SCROLL REVEAL ANIMATION ============== */
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  //reset: true
+})
+
+sr.reveal('.home__img, .new__container, .footer__container')
+sr.reveal('.home__data', {delay: 500})
+sr.reveal('.giving__content, .gift__card', {interval: 100})
+sr.reveal('.celebrate__data, .message__form, .footer__img1', {origin: 'left'})
+sr.reveal('.celebrate__img, .message__img, .footer__img2', {origin: 'right'})
